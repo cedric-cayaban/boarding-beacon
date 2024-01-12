7d3303bdf4115bf0c4f23ca8be6abfa59d108e59
cedric <cedricjoelcayaban15@gmail.com>
2024-01-13 00:49:03 +0800

    updated sql

--- boarding_beacon (1).sql
+++ boarding_beacon (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2023 at 12:21 AM
+-- Generation Time: Jan 12, 2024 at 05:48 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -100,14 +100,14 @@ CREATE TABLE `boarding` (
 --
 
 INSERT INTO `boarding` (`boardID`, `bName`, `addressID`, `bDescription`, `bPrice`, `bUnitNo`, `bImage`, `bImg1`, `bImg2`, `bImg3`, `bImg4`, `bImg5`, `bImg6`, `bStatus`) VALUES
-(9, 'Pagadan Ramos Apartelle', 'A29', 'A bustling boarding house with tenants that doesnt sleep, it has great hospitality and has spacious rooms', 6500, 6, 'Pagaduan.png', 'i1.png', 'i2.png', 'i3.png', 'i4.png', 'i5.png', 'i6.jpg', 'vacant'),
+(9, 'Pagadan Ramos ', 'A29', 'A bustling boarding house with tenants that doesnt sleep, it has great hospitality and has spacious rooms', 6500, 6, 'Pagaduan.png', 'i1.png', 'i2.png', 'i3.png', 'i4.png', 'i5.png', 'i6.jpg', 'Available'),
 (11, 'Michelle Apartelle', 'A15', '\"Welcome to Michelle Apartelle, where modern comfort meets convenience. Nestled in a vibrant community, our boarding house offers spacious rooms and a warm atmosphere, providing residents with a delightful and welcoming living experience', 4000, 1, 'Michelle.png', 'i7.jpg', 'i18.jpg', 'i9.jpg', 'i10.jpg', 'i11.jpg', 'i12.jpg', 'vacant'),
 (12, 'LTZ Boarding', 'A21', 'Step into the welcoming embrace of LTZ Boarding, where every corner exudes comfort and tranquility. Our thoughtfully designed rooms provide the perfect sanctuary for relaxation, and the vibrant community ', 7000, 7, 'ltz.png', 'i13.jpg', 'i14.jpeg', 'i15.jpg', 'i16.jpg', 'i17.jpg', 'i18.jpg', 'vacant'),
 (13, 'Taaca Village', 'A4', 'Embrace a new day at Taaca Village, where the morning sun illuminates our well-connected boarding house community. With spacious accommodations and a variety of amenities, ', 4444, 6, 'Taaca Village.png', 'i19.jpg', 'i20.jpg', 'i21.jpg', 'i17.jpg', 'i23.jpg', 'i24.jpg', 'vacant'),
 (14, 'DMI Urdaneta', 'A33', 'Find solace in the charm of DMI Urdaneta, a boarding house that offers more than just a place to stay. Our spacious accommodations provide a retreat from the bustling world', 5638, 3, 'DMI.png', 'i4.png', 'i13.jpg', 'i12.jpg', 'i21.jpg', 'i7.jpg', 'i11.jpg', 'vacant'),
-(15, 'Fidar VIlla', 'A34', 'Immerse yourself in the modern elegance of Urban Oasis Boarding House, where each room is a stylish sanctuary. With contemporary amenities and a central location', 8300, 1, 'fidar.png', 'i8.jpg', 'i1.png', 'i10.jpg', 'i19.jpg', 'i6.jpg', 'i24.jpg', 'vacant'),
-(16, 'Fidar VIlla', 'A34', 'Immerse yourself in the modern elegance of Urban Oasis Boarding House, where each room is a stylish sanctuary. With contemporary amenities and a central location', 8300, 1, 'fidar.png', 'i8.jpg', 'i1.png', 'i10.jpg', 'i19.jpg', 'i6.jpg', 'i24.jpg', 'vacant'),
-(17, 'Fidar VIlla', 'A34', 'Immerse yourself in the modern elegance of Urban Oasis Boarding House, where each room is a stylish sanctuary. With contemporary amenities and a central location', 8300, 1, 'fidar.png', 'i8.jpg', 'i1.png', 'i10.jpg', 'i19.jpg', 'i6.jpg', 'i24.jpg', 'vacant');
+(17, 'Fidar VIlla', 'A34', 'Immerse yourself in the modern elegance of Urban Oasis Boarding House, where each room is a stylish sanctuary. With contemporary amenities and a central location', 8300, 1, 'fidar.png', 'i8.jpg', 'i1.png', 'i10.jpg', 'i19.jpg', 'i6.jpg', 'i24.jpg', 'vacant'),
+(18, 'Sample Name', 'A1', 'sample desc', 1000, 3, 'Taaca Village.png', 'i13.jpg', 'i23.jpg', 'i4.png', 'i18.jpg', 'i9.jpg', 'i14.jpeg', 'vacant'),
+(39, 'sample11111', 'A1', 'sample', 1234, 12, 'Green simple business model canvas poster.png', 'i24.jpg', 'i21.jpg', 'i14.jpeg', 'i7.jpg', 'i1.png', 'Taaca Village.png', 'Available');
 
 -- --------------------------------------------------------
 
@@ -122,7 +122,9 @@ CREATE TABLE `owner` (
   `password` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `midname` varchar(255) NOT NULL,
+  `sex` varchar(255) NOT NULL,
   `contactNo` int(13) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `permit` varchar(255) NOT NULL,
   `accstatus` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -131,10 +133,11 @@ CREATE TABLE `owner` (
 -- Dumping data for table `owner`
 --
 
-INSERT INTO `owner` (`ownerID`, `fname`, `lname`, `password`, `username`, `midname`, `contactNo`, `permit`, `accstatus`) VALUES
-(16, 'tope', 'carpio', '12345Qwer', 'topetope', 'callo', 12345, 'fsasdfasdf', 'approved'),
-(17, 'Carpio', 'Christopherson', '12345Qwer', 'topetope', 'C', 2147483647, 'bh.png', ''),
-(20, 'cayaban', 'cedric', 'Cedced69', 'cedced', 'f', 2147483647, 'b2.png', 'approved');
+INSERT INTO `owner` (`ownerID`, `fname`, `lname`, `password`, `username`, `midname`, `sex`, `contactNo`, `address`, `permit`, `accstatus`) VALUES
+(32, 'cayaban', 'cedric', 'Cedced123', 'cedced', 'f', '', 2147483647, '', 'cayaban_assign_im.pdf', 'approved'),
+(43, 'biag', 'mak', 'Makmak123', 'makmak', 'b', '', 2147483647, '', 'White Abstract Graphic Design Portfolio Cover A4 Document (5).png', 'approved'),
+(46, 'mak', 'biag', 'Makmak123', 'makmak', 'b', 'Male', 2147483647, 'MACARANG, MANGATAREM, 2413', 'White Abstract Graphic Design Portfolio Cover A4 Document (4).png', 'pending'),
+(47, 'Carpio', 'Christopherson', 'Topetope123', 'topetope', 'C', 'Male', 2147483647, 'MACARANG, MANGATAREM, 2413', 'White Abstract Graphic Design Portfolio Cover A4 Document (5).png', 'pending');
 
 -- --------------------------------------------------------
 
@@ -149,21 +152,18 @@ CREATE TABLE `tenant` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `midname` varchar(255) NOT NULL,
-  `contactNo` int(13) NOT NULL
+  `sex` varchar(255) NOT NULL,
+  `contactNo` int(13) NOT NULL,
+  `address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tenant`
 --
 
-INSERT INTO `tenant` (`tenantID`, `fname`, `lname`, `username`, `password`, `midname`, `contactNo`) VALUES
-(44, 'fsf', 'fsf', 'fdsaffds', 'fsafsd434S', 'c', 32323),
-(45, '', '', '', 'fsafsd434S', '', 0),
-(46, '', '', '', 'FSDAFDS', '', 0),
-(47, 'Cayaban', 'Cedric', 'ceddybear', 'Cedced123', 'C', 123456),
-(48, 'carpio', 'tope', 'topetope', '12345Qwer', 'c', 1234),
-(49, 'biag', 'mak', 'makmak', 'Makmak69', 'b', 82194673),
-(50, 'Carpio', 'Christopherson', 'topetope', '12345Qwer', 'C', 2147483647);
+INSERT INTO `tenant` (`tenantID`, `fname`, `lname`, `username`, `password`, `midname`, `sex`, `contactNo`, `address`) VALUES
+(50, 'Carpio', 'Christopherson', 'topetope', '12345Qwer', 'C', '', 2147483647, ''),
+(51, 'cayaban', 'biag', 'janela', 'Tope12345', 'b', '', 2147483647, '');
 
 --
 -- Indexes for dumped tables
@@ -202,19 +202,19 @@ ALTER TABLE `tenant`
 -- AUTO_INCREMENT for table `boarding`
 --
 ALTER TABLE `boarding`
-  MODIFY `boardID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `boardID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `owner`
 --
 ALTER TABLE `owner`
-  MODIFY `ownerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `ownerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `tenant`
 --
 ALTER TABLE `tenant`
-  MODIFY `tenantID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `tenantID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- Constraints for dumped tables
